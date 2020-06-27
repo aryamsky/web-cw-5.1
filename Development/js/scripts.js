7c999a284c0c98c5fef77a9f4388c36302ca0ba1
Aryam Khalid <aryamkhalid05@gmail.com>
2020-06-27 21:44:04 +0300

    تم الحل

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,50 @@
 // السؤال الأول
 // 1
-
+const name="اريام"
 // 2
-
+let age=15;
 // 3 and 4
-
+console.log( "انا" + name + " " + "عمري" + age);
 // 5 and 6
+let five_times_age = 5*age;
+console.log(five_times_age)
 
+age *=5
+console.log(age);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+let hobbies = ["البرمجه" + "السباحه" + "القراءه"]
 
-// 2
 
+//
+hobbies.push(16)
+hobbies[4] = 16
+console.log(hobbies.lenght)
 // 3
-
+hobbies.pop()
+console.log(hobbies)
 // 4
-
+let student = {
+    track: "مسار الويب",
+    language:"JavaScript",
+}
 // 5
-
+student["TAs"] = ["نانسي" , "موضي" , "حسين" , "رهف"]
+console.log(student)
+console.log(student["track"])
 // 6
 
-// 7
+
+console.log(student["TAs"] [0])
+
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
-
+student["sayHello"] = function() {console.log("اريام")}
 // 2
 
 // console results => console.png
